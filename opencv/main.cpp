7285ab29b4cdf1dbc4202f272d1d3b482423f2e6
NaCl96 <13163062832@163.com>
2019-04-26 19:50:56 +0800

    combination and quick_sort

--- opencv/main.cpp
+++ opencv/main.cpp
@@ -25,6 +25,52 @@ void combination(vector<vector<int>> &res,vector<int> &candidates,int start,int 
 	}
 }
 
+//快速排序方法一
+void quick_sort1(vector<int> &nums, int left, int right) {
+	if (left < right) {
+		int i = left, j = left;
+		int pivot = nums[right];
+
+		for (j=left; j < right;j++) {
+			if (nums[j] < pivot) {
+				int tmp = nums[j];
+				nums[j] = nums[i];
+				nums[i] = tmp;
+				i++;
+			}
+		}
+		nums[j] = nums[i];
+		nums[i] = pivot;
+		quick_sort1(nums, left, i - 1);
+		quick_sort1(nums, i + 1, right);
+	}
+}
+
+//快速排序方法二
+void quick_sort2(vector<int> &nums, int left, int right) {
+	if (left < right) {
+		int i = left, j = right;
+		int pivot = nums[right];
+		while (i < j&&nums[i] <= pivot) {
+			i++;
+		}
+		while (i < j) {
+			nums[j--] = nums[i];
+		}
+		while (i < j&&nums[j] >= pivot) {
+			j--;
+		}
+		while (i < j) {
+			nums[i++] = nums[j];
+		}
+		nums[i] = pivot;
+		quick_sort2(nums, left, i - 1);
+		quick_sort2(nums, i + 1, right);
+	}
+	
+}
+
+
 int main() {
 	vector<int> candidates = { 2,3,6,7 };
 	int target = 7;
